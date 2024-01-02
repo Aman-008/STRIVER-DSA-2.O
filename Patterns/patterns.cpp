2ab18c1301192bd06e5cda2ec7deb7ca43cc3930
Aman <120547566+Aman-008@users.noreply.github.com>
2024-01-02 20:26:53 +0530

    commited

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -245,7 +245,16 @@ void pattern15(int n){
         }
         cout << endl;
     }
-
+}
+void pattern16(int n){
+    for(int i=0;i<n;i++){
+        char ch = 'A' + i;
+        for(int j=0;j<=i;j++){
+            cout << ch ;
+        }
+        
+        cout << endl;
+    }
 }
 int main()
 {
@@ -255,7 +264,7 @@ int main()
     {
         int n;
         cin >> n;
-        pattern15(n);
+        pattern16(n);
     }
 
     return 0;
